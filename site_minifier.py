c22edfad5d6c835664b61f144da04ea422f49111
William Y. Feng <womogenes2@gmail.com>
2023-05-19 16:42:23 -0700

    Minify data for the site

---
+++ site_minifier.py
@@ -0,0 +1,15 @@
+# Minify JSON files to be served to client
+# Needed because some of the existing JSON has whitespace to be human-readable
+
+import json
+import os
+
+files_to_minify = ["2.5k_artist_data.json",
+                   "2.5k_id2name.json", "2.5k_pca_log.json", "2.5k_artist_freq.json"]
+
+for file in files_to_minify:
+    print(f"Minifying {file}...")
+    with open(f"./data/{file}") as fin:
+        data = json.load(fin)
+    with open(f"./site/data/{file}", "w") as fout:
+        json.dump(data, fout)
--- utils.py
+++ utils.py
@@ -5,11 +5,24 @@ with open("./data/name_id_maps.json") as fin:
     name2id = data["name2id"]
     id2name = data["id2name"]
 
-with open("./data/2.5k_artist_freq.json") as fin:
+with open("./data/artist_freq.json") as fin:
     artist_freq_dict = json.load(fin)
 
     N = 2500
-    top_artists = [a[0] for a in sorted(artist_freq_dict.items(), key=lambda x: -x[1])][:N]
+    top_artists = [a[0] for a in sorted(
+        artist_freq_dict.items(), key=lambda x: -x[1])][:N]
 
 with open("./data/2.5k_artist_data.json") as fin:
     artist_data = json.load(fin)
+
+
+# Subset slices
+with open("./data/2.5k_id2name.json", "w") as fout:
+    id2name_slice = {artist_id: id2name[artist_id]
+                     for artist_id in top_artists}
+    json.dump(id2name_slice, fout, indent=2)
+
+with open("./data/2.5k_artist_freq.json", "w") as fout:
+    artist_freq_slice = {
+        artist_id: artist_freq_dict[artist_id] for artist_id in top_artists}
+    json.dump(artist_freq_slice, fout)
