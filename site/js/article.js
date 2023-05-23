48079fde33875cc534c8da29bb67a55ae4227f8e
William Y. Feng <womogenes2@gmail.com>
2023-05-23 10:58:42 -0700

    Add terrible Markdown rendering

---
+++ site/js/article.js
@@ -0,0 +1,18 @@
+// Inject Markdown!
+const content = await (await fetch('../writing_stuff/about_data.md')).text();
+document.querySelector('#article-container').innerHTML = marked.parse(content);
+
+document.addEventListener('DOMContentLoaded', function () {
+  renderMathInElement(document.body, {
+    // customised options
+    // auto-render specific keys, e.g.:
+    delimiters: [
+      { left: '$$', right: '$$', display: true },
+      { left: '$', right: '$', display: false },
+      { left: '\\(', right: '\\)', display: false },
+      { left: '\\[', right: '\\]', display: true },
+    ],
+    // rendering keys, e.g.:
+    throwOnError: false,
+  });
+});
