ad0d6dd3be8f3332ca32cc786ae28994ffa0ef55
William Y. Feng <womogenes2@gmail.com>
2023-05-23 17:51:58 -0700

    Successfully integrate KaTeX

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -1,5 +1,30 @@
+const katex = require('katex');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('interactive/js');
   eleventyConfig.addPassthroughCopy('interactive/data');
+
+  eleventyConfig.addFilter('latex', (content) => {
+    // https://benborgers.com/posts/eleventy-katex
+    const cleanEquation = (equation) =>
+      equation.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+
+    const displayRegex = /\$\$\s*(.+?)\s*\$\$/g;
+    const inlineRegex = /\$\s*(.+?)\s*\$/g;
+
+    return content
+      .replace(displayRegex, (_, equation) => {
+        return katex.renderToString(cleanEquation(equation), {
+          throwOnError: false,
+          displayMode: true,
+        });
+      })
+      .replace(inlineRegex, (_, equation) => {
+        return katex.renderToString(cleanEquation(equation), {
+          throwOnError: false,
+          displayMode: false,
+        });
+      });
+  });
 };
