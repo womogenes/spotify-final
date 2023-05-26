59ff06ccfa196cdf71c9aa54d39806335a9886c9
Cecilia Sun <ceciliaminions@gmail.com>
2023-05-25 17:30:08 -0700

    change colors

--- site/interactive/js/genre_colors.js
+++ site/interactive/js/genre_colors.js
@@ -3,17 +3,18 @@ import Color from 'https://colorjs.io/dist/color.js';
 import { artistData, genreMap } from './data_export.js';
 
 // "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+// "#ea5545", "#f46a9b", "#8c564b", "#ef9b20", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"
 
 let bigGenreColors = {
-  'hip hop': '#f542cb',
-  rap: '#9467bd',
-  pop: '#4256ed',
-  edm: '#2b7fed',
-  indie: '#0dd0d4',
-  soul: '#1dc70e',
-  jazz: '#afd40d',
-  rock: '#fcba03',
-  country: '#d62215',
+  'hip hop': '#ea5545',
+  rap: '#f46a9b',
+  pop: '#8c564b',
+  edm: '#ef9b20',
+  indie: '#ede15b',
+  soul: '#bdcf32',
+  jazz: '#87bc45',
+  rock: '#27aeef',
+  country: '#b33dc6',
   other: '#eeeeee',
 };
 
